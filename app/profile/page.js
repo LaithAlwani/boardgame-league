81e94da5fe901c39af2fd120fb91865dc8850ad3
laith <laithalwani@gmail.com>
2025-08-14 12:26:25 -0400

    fix build error

--- app/profile/page.js
+++ app/profile/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Profile() {
   return (
     <div>Profile
-      <p>This Page show's users settings, thier leageus and upcoming matches</p>
+      <p>This Page show&apos;s users settings, thier leageus and upcoming matches</p>
     </div>
   )
 }
